c58f37dde0f1fa6701a89f0315f46ab2055e699e
Michael Lux <michael.lux@aisec.fraunhofer.de>
2021-06-01 21:07:44 +0200

    ++

--- tally.py
+++ tally.py
@@ -23,30 +23,27 @@ async def connect(tally_cam: int, host: str, port: int) -> (StreamReader, Stream
 
 
 async def watch(cam: int, tally_cam: int, callback: Callable[[int, int], Awaitable[None]], host: str, port: int):
-    # Start with only 100 ms reconnect delay on first error
-    reconnect_delay_ms = 50
+    # Start with only 1 second reconnect delay on first error (see doubling below)
+    reconnect_delay = 0.5
     LOG.info("Connecting to tally state monitoring for device %d (PTZ %d)" % (tally_cam, cam))
+    writer = None
     while True:
         try:
-            if reconnect_delay_ms > 50:
-                # Convert reconnect delay to seconds
-                delay_s = reconnect_delay_ms / 1000
-                LOG.error("Error for device %d, try reconnect after %.2f s..." % (tally_cam, delay_s))
-                await asyncio.sleep(delay_s)
+            if reconnect_delay > 0.5:
+                LOG.error("Error for device %d, try reconnect after %d s..." % (tally_cam, reconnect_delay))
+                await asyncio.sleep(reconnect_delay)
             reader, writer = await connect(tally_cam, host, port)
             while True:
-                state_bytes = await reader.read(1)
-                if len(state_bytes) == 0:
-                    # Close possibly broken connection, do not await result (wait_close())
-                    writer.close()
-                    raise Exception("Received empty tally response for PTZ %d" % cam)
-                else:
-                    # Upon success, reset reconnect delay to initial value
-                    reconnect_delay_ms = 50
+                state_bytes = await reader.readexactly(1)
+                # Upon success, reset reconnect delay to initial value
+                reconnect_delay = 0.5
                 state = int.from_bytes(state_bytes, 'big')
                 LOG.info("Received state %d for PTZ %d" % (state, cam))
                 await callback(cam, state)
         except Exception as e:
             logging.exception(e)
+            if writer is not None:
+                # Close possibly broken connection, do not await result (wait_close())
+                writer.close()
             # Increase delay exponentially
-            reconnect_delay_ms = min(reconnect_delay_ms * 2, 20000)
+            reconnect_delay = min(reconnect_delay * 2, 20)
